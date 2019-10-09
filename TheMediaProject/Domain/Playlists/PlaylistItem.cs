a52637577edffbd3ab8c48ce3c53d72c4d2900fc
Simon Brondeel <simonbrondeell@gmail.com>
2019-10-09 09:12:31 +0200

    Playlist Serverside

---
+++ TheMediaProject/Domain/Playlists/PlaylistItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheMediaProject.Domain.Playlists
+{
+    public class PlaylistItem
+    {
+        public int PlaylistId { get; set; }
+        public int MediaId { get; set; }
+        public enum MediaType { Movie, Series}
+        public MediaType Type { get; set; }
+    }
+}
